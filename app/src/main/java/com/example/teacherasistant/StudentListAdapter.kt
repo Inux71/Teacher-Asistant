e2c2b02255387e413699154ddadc7a1138522701
Kacper Grabiec <grabiec.kacper01@gmail.com>
2022-12-09 15:20:01 +0100

    add student_list fragment

--- app/src/main/java/com/example/teacherasistant/StudentListAdapter.kt
+++ app/src/main/java/com/example/teacherasistant/StudentListAdapter.kt
@@ -6,23 +6,23 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class SubjectListAdapter() : RecyclerView.Adapter<SubjectListAdapter.ViewHolder>() {
+class StudentListAdapter : RecyclerView.Adapter<StudentListAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val dayTextView: TextView
-        val timeTextView: TextView
-        val nameTextView: TextView
+        val idTextView: TextView
+        val firstNameTextView: TextView
+        val lastNameTextView: TextView
 
         init {
-            dayTextView = view.findViewById(R.id.subject_list_item_day_textview)
-            timeTextView = view.findViewById(R.id.subject_list_item_time_textview)
-            nameTextView = view.findViewById(R.id.subject_list_item_name_textview)
+            idTextView = view.findViewById(R.id.student_list_item_id_textview)
+            firstNameTextView = view.findViewById(R.id.student_list_item_firstname_textview)
+            lastNameTextView = view.findViewById(R.id.student_list_item_lastname_textview)
         }
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.subject_list_item, viewGroup, false)
+            .inflate(R.layout.student_list_item, viewGroup, false)
 
         return ViewHolder(view)
     }
